449675dbe2b4f87ff3b3d670f2f286d85ff70da3
fadarash <fadarash@gmail.com>
2022-11-30 12:04:16 +0330

    redirect

--- pages/news/[...slug].tsx
+++ pages/news/[...slug].tsx
@@ -1,25 +1,27 @@
-import { useRouter } from 'next/router'
-import Link from 'next/link'
-import styles from '../../styles.module.css'
-import Code from '../../components/Code'
+import { useRouter } from "next/router";
+import Link from "next/link";
+import styles from "../../styles.module.css";
+import Code from "../../components/Code";
 
 export default function News() {
-  const { asPath, route, query } = useRouter()
-
+  const { asPath, route, query } = useRouter();
+  console.log(query);
   return (
     <div className={styles.container}>
       <div className={styles.card}>
-        <h1>Path: {asPath}</h1>
+        <h1>asPath: {asPath}</h1>
+        <h1>route: {route}</h1>
+        <h1>query.slug: {JSON.stringify(query.slug)}</h1>
         <hr className={styles.hr} />
         <p>
-          This page was rendered by <Code>{`pages${route}.tsx`}</Code>.
+          This page was rendered by <Code>route:{`pages${route}.tsx`}</Code>.
         </p>
         <p>
-          The query <Code>slug</Code> for this page is:{' '}
+          The query <Code>slug</Code> for this page is:{" "}
           <Code>{JSON.stringify(query.slug)}</Code>
         </p>
         <Link href="/">&larr; Back home</Link>
       </div>
     </div>
-  )
+  );
 }
