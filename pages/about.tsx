791a8a02c816d8b22bc7b10429170c2879270625
fadarash <fadarash@gmail.com>
2022-12-07 08:58:55 +0330

    typescirpt

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,35 +1,14 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Link from '../src/Link';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
+import Link from 'next/link'
+import Layout from '../components/Layout'
 
-export default function About() {
-  return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="h4" component="h1" gutterBottom>
-          MUI v5 + Next.js with TypeScript example
-        </Typography>
-        <Box maxWidth="sm">
-          <Button variant="contained" component={Link} noLinkStyle href="/">
-            Go to the home page
-          </Button>
-        </Box>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  );
-}
+const AboutPage = () => (
+  <Layout title="About | Next.js + TypeScript Example">
+    <h1>About</h1>
+    <p>This is the about page</p>
+    <p>
+      <Link href="/">Go home</Link>
+    </p>
+  </Layout>
+)
+
+export default AboutPage
